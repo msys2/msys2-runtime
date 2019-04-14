82c754e4eccb3c273bdafd136b9591658c4158f2
Алексей Павлов <alexey.pawlow@gmail.com>
2019-04-14 21:17:46 +0300

    Add functionality for converting UNIX paths in arguments and environment variables to Windows form for native Win32 applications.

--- winsup/cygwin/winf.h
+++ winsup/cygwin/winf.h
@@ -51,6 +51,10 @@ class av
 	calloced = 1;
       }
   }
+  void replace (int i, const char *arg)
+  {
+    argv[i] = cstrdup1 (arg);
+  }
   void dup_all ()
   {
     for (int i = calloced; i < argc; i++)
