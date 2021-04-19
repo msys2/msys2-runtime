2051fdf6e9703323ac53b27612fb1c55e88f1a40
Corinna Vinschen <corinna@vinschen.de>
2021-04-19 14:49:14 +0200

    Cygwin: path_conv: Try to handle native symlinks more sanely
    For local paths, add a check if the inner path components contain native
    symlinks or junctions.  Compare the incoming path with the path returned
    by NtQueryInformationFile(FileNameInformation).  If they differ, there
    must be at least one native symlink or junction in the path.  If so,
    treat the currently evaluated file as non-existant.  This forces
    path_conv::check to backtrack inner path components until we eliminated
    all native symlinks or junctions and have a normalized path.
    
    Signed-off-by: Corinna Vinschen <corinna@vinschen.de>
    (cherry picked from commits 456c3a46386f38887407603b2c64b7f63a4871c5 and
    13fd26ecf5ca8417146d57b45aed0133435c3497)

--- winsup/cygwin/path.cc
+++ winsup/cygwin/path.cc
@@ -3341,6 +3341,58 @@ restart:
 	  status = conv_hdl.get_finfo (h, fs.is_nfs ());
 	  if (NT_SUCCESS (status))
 	    fileattr = conv_hdl.get_dosattr (fs.is_nfs ());
+
+	  /* For local paths, check if the inner path components contain
+	     native symlinks or junctions.  Compare incoming path with
+	     path returned by NtQueryInformationFile(FileNameInformation).
+	     If they differ, bail out as if the file doesn't exist.  This
+	     forces path_conv::check to backtrack inner path components. */
+	  if (!fs.is_remote_drive ())
+	    {
+#ifdef __i386__
+	      /* On WOW64, ignore any potential problems if the path is inside
+		 the Windows dir to avoid false positives for stuff under
+		 File System Redirector control. */
+	      if (wincap.is_wow64 ())
+		{
+		  static UNICODE_STRING wpath;
+		  UNICODE_STRING udpath;
+
+		  /* Create UNICODE_STRING for Windows dir. */
+		  RtlInitCountedUnicodeString (&wpath, windows_directory,
+				windows_directory_length * sizeof (WCHAR));
+		  /* Create a UNICODE_STRING from incoming path, splitting
+		     off the leading "\\??\\" */
+		  RtlInitCountedUnicodeString (&udpath, upath.Buffer + 4,
+				upath.Length - 4 * sizeof (WCHAR));
+		  /* Are we below Windows dir?  Skip the check for inner
+		     symlinks. */
+		  if (RtlEqualUnicodePathPrefix (&udpath, &wpath, TRUE))
+		    goto skip_inner_syml_check;
+		}
+#endif /* __i386__ */
+	      PFILE_NAME_INFORMATION pfni;
+
+	      pfni = (PFILE_NAME_INFORMATION) tp.c_get ();
+	      if (NT_SUCCESS (NtQueryInformationFile (h, &io, pfni, NT_MAX_PATH,
+						      FileNameInformation)))
+		{
+		  UNICODE_STRING npath;
+
+		  RtlInitCountedUnicodeString (&npath, pfni->FileName,
+					      pfni->FileNameLength);
+		  if (!RtlEqualUnicodePathSuffix (&upath, &npath, !!ci_flag))
+		    {
+		      fileattr = INVALID_FILE_ATTRIBUTES;
+		      set_error (ENOENT);
+		      break;
+		    }
+		}
+#ifdef __i386__
+	      skip_inner_syml_check:
+	        ;
+#endif /* __i386__ */
+	    }
 	}
       if (!NT_SUCCESS (status))
 	{
