Alexpux/Cygwin

kill: kill Win32 processes more gently
This change is the equivalent to the change to the Ctrl+C handling we
just made.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

winsup/utils/kill.cc
@@ -17,6 +17,7 @@ details. */
 #include <cygwin/version.h>
 #include <getopt.h>
 #include <limits.h>
+#include <cygwin/exit_process.h>
 
 static char *prog_name;
 
@@ -171,10 +172,28 @@ forcekill (int pid, int sig, int wait)
       return;
     }
   if (!wait || WaitForSingleObject (h, 200) != WAIT_OBJECT_0)
-    if (sig && !TerminateProcess (h, sig << 8)
-	&& WaitForSingleObject (h, 200) != WAIT_OBJECT_0)
-      fprintf (stderr, "%s: couldn't kill pid %u, %u\n",
+    {
+      if (sig == SIGINT || sig == SIGTERM)
+        {
+	  HANDLE cur = GetCurrentProcess (), h2;
+	  /* duplicate handle with access rights required for exit_process() */
+	  if (DuplicateHandle (cur, h, cur, &h2, PROCESS_CREATE_THREAD |
+			       PROCESS_QUERY_INFORMATION |
+			       PROCESS_VM_OPERATION |
+			       PROCESS_VM_WRITE | PROCESS_VM_READ |
+			       PROCESS_TERMINATE, FALSE, 0))
+	    {
+	      exit_process (h2, 128 + sig);
+	      CloseHandle (h2);
+	    }
+	  else
+	    terminate_process_tree(h, 128 + sig);
+	}
+      else if (sig && !TerminateProcess (h, sig << 8)
+          && WaitForSingleObject (h, 200) != WAIT_OBJECT_0)
+        fprintf (stderr, "%s: couldn't kill pid %u, %u\n",
 	       prog_name, (unsigned) dwpid, (unsigned int) GetLastError ());
+    }
   CloseHandle (h);
 }
 

msys2/msys2-runtime

Co-authored-by: Naveen M K <naveen@syrusdark.website>

winsup/utils/kill.cc
@@ -17,6 +17,7 @@ details. */
 #include <cygwin/version.h>
 #include <getopt.h>
 #include <limits.h>
+#include <cygwin/exit_process.h>
 
 static char *prog_name;
 
@@ -188,10 +189,20 @@ forcekill (pid_t pid, DWORD winpid, int sig, int wait)
       return;
     }
   if (!wait || WaitForSingleObject (h, 200) != WAIT_OBJECT_0)
-    if (sig && !TerminateProcess (h, sig << 8)
-	&& WaitForSingleObject (h, 200) != WAIT_OBJECT_0)
-      fprintf (stderr, "%s: couldn't kill pid %u, %u\n",
-	       prog_name, (unsigned int) dwpid, (unsigned int) GetLastError ());
+    {
+      HANDLE cur = GetCurrentProcess (), h2;
+      /* duplicate handle with access rights required for exit_process_tree() */
+      if (DuplicateHandle (cur, h, cur, &h2, PROCESS_CREATE_THREAD |
+			      PROCESS_QUERY_INFORMATION |
+			      PROCESS_VM_OPERATION |
+			      PROCESS_VM_WRITE | PROCESS_VM_READ |
+			      PROCESS_TERMINATE, FALSE, 0))
+      {
+        CloseHandle(h);
+        h = h2;
+      }
+      exit_process_tree (h, 128 + sig);
+    }
   CloseHandle (h);
 }
 