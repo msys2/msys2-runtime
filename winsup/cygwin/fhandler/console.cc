fb060f79f664189f3b61a9d361dafbedf79e2441
Takashi Yano <takashi.yano@nifty.ne.jp>
2024-02-03 00:54:23 +0900

    Cygwin: console: Avoid slipping past disable_master_thread check.
    If disable_master_thread flag is set between the code checking that
    flag not be set and the code acquiring input_mutex, input record is
    processed once after setting disable_master_thread flag. This patch
    prevents that.
    
    Backported-from: 9bcfd06045 (Cygwin: console: Avoid slipping past disable_master_thread check., 2024-02-03)
    Fixes: d4aacd50e6cf ("Cygwin: console: Add missing input_mutex guard.")
    Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- winsup/cygwin/fhandler/console.cc
+++ winsup/cygwin/fhandler/console.cc
@@ -361,6 +361,12 @@ fhandler_console::cons_master_thread (handle_set_t *p, tty *ttyp)
 	}
 
       WaitForSingleObject (p->input_mutex, mutex_timeout);
+      /* Ensure accessing input recored is not disabled. */
+      if (con.disable_master_thread)
+	{
+	  ReleaseMutex (p->input_mutex);
+	  continue;
+	}
       total_read = 0;
       switch (cygwait (p->input_handle, (DWORD) 0))
 	{
@@ -4335,8 +4341,6 @@ fhandler_console::set_disable_master_thread (bool x, fhandler_console *cons)
       else
 	return;
     }
-  if (con.disable_master_thread == x)
-    return;
   cons->acquire_input_mutex (mutex_timeout);
   con.disable_master_thread = x;
   cons->release_input_mutex ();
