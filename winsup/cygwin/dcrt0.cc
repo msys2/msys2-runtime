5d8dd93bd26730226e9749ac1b0f257162decf57
Yonggang Luo <luoyonggang@gmail.com>
2026-01-16 10:43:58 +0800

    Revert "Rename dll from cygwin to msys"
    This reverts commit dae2b5bc659d8fdec13f6d7e864277bbbce9dd1a.

--- winsup/cygwin/dcrt0.cc
+++ winsup/cygwin/dcrt0.cc
@@ -1090,11 +1090,7 @@ dll_crt0 (per_process *uptr)
    See winsup/testsuite/cygload for an example of how to use cygwin1.dll
    from MSVC and non-cygwin MinGW applications.  */
 extern "C" void
-#ifdef __MSYS__
-msys_dll_init ()
-#else
 cygwin_dll_init ()
-#endif
 {
   static int _fmode;
 
--- winsup/utils/mingw/strace.cc
+++ winsup/utils/mingw/strace.cc
@@ -284,7 +284,7 @@ load_cygwin ()
   if (h)
     return 0;
 
-  if (!(h = LoadLibrary ("msys-2.0.dll")))
+  if (!(h = LoadLibrary ("cygwin1.dll")))
     {
       errno = ENOENT;
       return 0;
@@ -356,16 +356,17 @@ create_child (char **argv)
     printf ("create_child: %s\n", one_line.buf);
 
   SetConsoleCtrlHandler (NULL, 0);
-  const char *cygwin_env = getenv ("MSYS");
+
+  const char *cygwin_env = getenv ("CYGWIN");
   const char *space;
 
   if (cygwin_env && strlen (cygwin_env) <= 256) /* sanity check */
     space = " ";
   else
     space = cygwin_env = "";
-  char *newenv = (char *) malloc (sizeof ("MSYS=noglob")
+  char *newenv = (char *) malloc (sizeof ("CYGWIN=noglob")
 				  + strlen (space) + strlen (cygwin_env));
-  sprintf (newenv, "MSYS=noglob%s%s", space, cygwin_env);
+  sprintf (newenv, "CYGWIN=noglob%s%s", space, cygwin_env);
   _putenv (newenv);
   ret = CreateProcess (0, one_line.buf,	/* command line */
 		       NULL,	/* Security */
