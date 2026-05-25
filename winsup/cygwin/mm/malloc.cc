04f9e88ce7834c95d912548a0299c831abcc2db6
Jon Turney <jon.turney@dronecode.org.uk>
2026-05-25 23:44:17 +0100

    Cygwin: Variable 'nfences' computed but never used
    ../../../../src/winsup/cygwin/mm/malloc.cc: In function ‘void add_segment(mstate, char*, size_t, flag_t)’:
    ../../../../src/winsup/cygwin/mm/malloc.cc:3995:7: error: variable ‘nfences’ set but not used [-Werror=unused-but-set-variable=]

--- winsup/cygwin/mm/malloc.cc
+++ winsup/cygwin/mm/malloc.cc
@@ -3992,7 +3992,9 @@ static void add_segment(mstate m, char* tbase, size_t tsize, flag_t mmapped) {
   msegmentptr ss = (msegmentptr)(chunk2mem(sp));
   mchunkptr tnext = chunk_plus_offset(sp, ssize);
   mchunkptr p = tnext;
+#if DEBUG != 0
   int nfences = 0;
+#endif
 
   /* reset top to new space */
   init_top(m, (mchunkptr)tbase, tsize - TOP_FOOT_SIZE);
@@ -4010,7 +4012,9 @@ static void add_segment(mstate m, char* tbase, size_t tsize, flag_t mmapped) {
   for (;;) {
     mchunkptr nextp = chunk_plus_offset(p, SIZE_T_SIZE);
     p->head = FENCEPOST_HEAD;
+#if DEBUG != 0
     ++nfences;
+#endif
     if ((char*)(&(nextp->head)) < old_end)
       p = nextp;
     else
